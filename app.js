0684178dccef5a5d593e1c54a89554c9025e7c8b
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2024-12-21 13:53:42 -0400

    65. Parsing incoming requests

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-/**
- * This for regex start
- */
-app.use('/add-product', (req, res, next) => {
-    console.log("In add product middleware!");
-    res.status(200).send("<h1>The 'Add Product' Page</h1>");
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/add-product', (req, res) => {
+    res.status(200).send("<form action='/product' method='post'><input type='text' name='title'><button type='submit'>Add Product</button> </form>");
+})
+
+app.use('/product', (req, res) => {
+    console.log(req.body)
+    res.redirect('/');
 })
 
 app.use('/', (req, res, next) => {
-    console.log("In another middleware!");
     res.status(200).send("<h1>Hello From Express!</h1>");
 })
 
