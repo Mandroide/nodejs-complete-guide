eaf045e44f381a69f259a4bc833e6052eec8c7a8
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2024-12-22 18:22:43 -0400

    99. Adding Controllers

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const adminData = require('./routes/admin');
+const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 const bodyParser = require('body-parser');
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 
 app.use(shopRouter);
 
-app.use('/admin', adminData.router);
+app.use('/admin', adminRouter);
 
 app.use((req, res, next) => {
     res.status(404).render("not-found", {
--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,12 @@
 const express = require('express')
 
 const router = express.Router();
+const productsController = require("../controllers/products");
 
-const products = []
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct)
 
-router.get('/add-product', (req, res) => {
-    res.render('add-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/add-product'
-    });
-})
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct)
 
-router.post('/add-product', (req, res) => {
-    products.push({title: req.body.title});
-    res.redirect('/');
-})
-
-exports.router = router;
-exports.products = products;
+module.exports = router;
