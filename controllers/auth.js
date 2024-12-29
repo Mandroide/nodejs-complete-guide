327ee4e1646befe07c64d28e1447c55176ae431d
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2024-12-29 12:10:24 -0400

    245. Fixing some minor bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,3 +30,10 @@ exports.postLogin = (req, res) => {
             console.log(err);
         });
 };
+
+exports.postLogout = (req, res) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/login');
+    });
+};
